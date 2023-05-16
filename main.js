0ebb4548e0f3eb7cbe63820550471ed2ee93539a
Jon Vadillo <vadillo.jon@gmail.com>
2023-05-16 16:36:44 +0200

    Basic usage

---
+++ main.js
@@ -0,0 +1,32 @@
+const API_URL = "https://www.balldontlie.io/api/v1/players"
+
+fetch(API_URL)
+    .then(response => response.json())
+    .then( json => {
+        addRows(json.data);
+    });
+
+function addRows(players) {
+    tbody = document.getElementById("tbody");
+    players.forEach(element => {
+        tbody.appendChild(createPlayerRow(element))
+    });
+}
+
+function createPlayerRow(player){
+    let row = document.createElement("tr")
+
+    let name = document.createElement("td")
+        name.textContent = player.first_name;
+        row.appendChild(name);
+
+    let lastname = document.createElement("td");
+        lastname.textContent = player.last_name;
+        row.appendChild(lastname);
+
+    let team = document.createElement("td")
+        team.textContent = player.team.name;
+        row.appendChild(team);
+        
+    return row;
+}
